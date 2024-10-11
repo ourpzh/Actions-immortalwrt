b00ecb51e9820a37fdd203d334cb9f2067e86352
ourpzh <1234758@qq.com>
2024-10-11 17:18:00 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -8,7 +8,7 @@ rm -rf ./feeds/packages/net/chinadns-ng
 
 
 #克隆依赖插件
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
+# git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
 
 
 #克隆的源码放在small文件夹
@@ -17,9 +17,9 @@ pushd package/small
 
 
 #adguardhome
-git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
+# git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
 #lucky
-git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
+# git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
 # #smartdns
 # git clone -b lede --depth 1 https://github.com/pymumu/luci-app-smartdns.git
 # git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
@@ -28,7 +28,7 @@ git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
 # #passwall
 # git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
 #passwall2
-git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
+# git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
 # #mosdns
 # git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
 # #openclash
