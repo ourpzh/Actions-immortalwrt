8841e2737ffd308ea46eb53a2122974fe6f231b1
ourpzh <1234758@qq.com>
2024-10-11 17:15:46 +0800

    Update diy.sh

--- scripts/master/diy.sh
+++ scripts/master/diy.sh
@@ -9,7 +9,7 @@ rm -rf ./feeds/packages/net/chinadns-ng
 rm -rf package/boot/uboot-rockchip/src/arch/arm/dts/rk3328-orangepi-r1-plus-lts-u-boot.dtsi
 
 #更改默认地址为192.168.8.1
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.153.222/g' package/base-files/files/bin/config_generate
 
 #更改r8152默认驱动
 #sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/g' target/linux/rockchip/image/armv8.mk
@@ -18,7 +18,7 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 sed -i '$a CONFIG_PHY_ROCKCHIP_INNO_USB3=y' target/linux/rockchip/armv8/config-6.1
 
 #克隆passwall环境插件
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
+#git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
 
 #克隆的源码放在small文件夹
 mkdir package/small
@@ -26,12 +26,12 @@ pushd package/small
 
 #克隆源码
 #adguardhome
-git clone -b main --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
+#git clone -b main --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
 #passwall2
-git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
+#git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
 #mosdns
-git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+#git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
 #lucky
-git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
+#git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
 
 popd
